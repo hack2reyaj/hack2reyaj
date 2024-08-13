5bd7f6fa5a42dcc6f58cd4ce411b3c58eeab7404
way2reyaj <way2reyaj1998@gmail.com>
2024-08-13 21:29:03 +0545

    ch

--- src/features/Home.js
+++ src/features/Home.js
@@ -1,8 +1,33 @@
-import React from 'react'
+
+import React, { useState } from 'react'
 
 const Home = () => {
+
+  const [text, setText] = useState('first value')
+
+
+
+
+
+  const handleChange = (msg) => {
+    setText((prev) => {
+      console.log(prev);
+      return msg;
+
+    });
+
+
+  }
+
+  console.log('render');
+
+
   return (
-    <div>
+    <div className="p-2">
+
+      <button onClick={() => handleChange('lio')} className="p-2 border-4 border-black  m-4 w-28 bg-black text-white font-serif font-bold hover:bg-blue-900 rounded-lg">Click me</button>
+
+      <h1>Hello text----- {text}</h1>
 
     </div>
   )
